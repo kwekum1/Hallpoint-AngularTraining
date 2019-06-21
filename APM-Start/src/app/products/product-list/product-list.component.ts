f256bc6b78ba22ca9ccfe969b6f73fd3aaaf228d
Kweku Martin <KMARTIN@maky.midam.farm>
2019-06-21 11:38:31 -0400

    Add changes to complete through module 7

---
+++ APM-Start/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IProduct } from './product';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  
+ 
+
+  constructor() { 
+this.filteredProducts = this.products;
+this.listFilter = 'cart';
+
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
+
+  toggleImage(): void {  
+    this.showImage = !this.showImage;
+
+  }
+
+  pageTitle: string = 'Product List';
+  showImage: boolean = true;
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value:string){
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+
+  filteredProducts: IProduct[];
+  products: IProduct[] = [  {
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2016",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+  },
+  {
+    "productId": 2,
+    "productName": "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2016",
+    "description": "15 gallon capacity rolling garden cart",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+  }];
+
+  performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+}
